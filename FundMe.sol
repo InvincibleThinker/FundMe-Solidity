0c4ca42b56141a6113004321e8518b2dac846fce
amar <amarkodge@gmail.com>
2025-01-05 01:48:28 +0530

    actually withdraw balance using transfer, send, call methods

--- FundMe.sol
+++ FundMe.sol
@@ -24,6 +24,21 @@ contract FundMe {
             address funder = funders[funderIndex];
             adressToAmountFunded[funder] = 0;
         }
+        //reset the array
+        funders = new address[](0);
+        //actually withdraw the balance
+
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+
+        //call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
     }
 
     
